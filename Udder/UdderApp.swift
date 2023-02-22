24e187f3bd76bc0ee41bec723e6774f503331971
Tingg123 <lidanting123@gmail.com>
2023-02-22 14:13:47 -0500

    Initial Commit

---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by Danting on 2/22/23.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
